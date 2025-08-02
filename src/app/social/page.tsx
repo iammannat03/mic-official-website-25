08b443e22a1dd2ccb7dec342fa2f66fa200103df
GR <grvig06@gmail.com>
2025-08-02 20:15:16 +0530

    laid out placeholder links for the icons

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -1,5 +1,52 @@
 import Image from "next/image";
 
+const clouds = [
+  { src: "/images/cloud1.png", alt: "Cloud", width: 100, height: 50, style: { top: 60, left: 200 } },
+  { src: "/images/cloud2.png", alt: "Cloud", width: 80, height: 40, style: { top: 120, left: 600 } },
+  { src: "/images/cloud3.png", alt: "Cloud", width: 110, height: 55, style: { top: 300, left: 400 } },
+  { src: "/images/cloud1.png", alt: "Cloud", width: 90, height: 45, style: { top: 400, left: 800 } },
+  { src: "/images/cloud2.png", alt: "Cloud", width: 80, height: 40, style: { top: 200, left: 1000 } },
+  { src: "/images/cloud1.png", alt: "Cloud", width: 60, height: 30, style: { top: 500, left: 100 } },
+  { src: "/images/cloud2.png", alt: "Cloud", width: 60, height: 30, style: { top: 600, left: 900 } },
+];
+
+const pipes = [
+  { left: 160 },
+  { left: 420 },
+  { left: 650 },
+  { left: 900 },
+];
+
+const socialIcons = [
+  {
+    href: "mailto:your@email.com",
+    src: "/images/mail.png",
+    alt: "Email",
+    width: 60,
+    height: 45,
+    style: { top: 180, left: 100, zIndex: 2 },
+    aria: "Send Email",
+  },
+  {
+    href: "https://instagram.com/",
+    src: "/images/instagram.png",
+    alt: "Instagram",
+    width: 60,
+    height: 60,
+    style: { top: 120, left: 470, zIndex: 2 },
+    aria: "Instagram",
+  },
+  {
+    href: "https://linkedin.com/",
+    src: "/images/linkedin.png",
+    alt: "LinkedIn",
+    width: 60,
+    height: 60,
+    style: { top: 80, left: 720, zIndex: 2 },
+    aria: "LinkedIn",
+  },
+];
+
 export default function SocialPage() {
   return (
     <div
@@ -25,37 +72,80 @@ export default function SocialPage() {
       />
 
       {/* Clouds */}
-      <Image src="/images/cloud1.png" alt="cloud" width={200} height={100} style={{ position: "absolute", top: 60, left: 200 }} />
-      <Image src="/images/cloud2.png" alt="cloud" width={150} height={90} style={{ position: "absolute", top: 120, left: 600 }} />
-      <Image src="/images/cloud3.png" alt="cloud" width={210} height={100} style={{ position: "absolute", top: 300, left: 400 }} />
-      <Image src="/images/cloud1.png" alt="cloud" width={300} height={200} style={{ position: "absolute", top: 400, left: 800 }} />
-      <Image src="/images/cloud2.png" alt="cloud" width={200} height={100} style={{ position: "absolute", top: 200, left: 1000 }} />
+      {clouds.map((cloud, i) => (
+        <Image
+          key={i}
+          src={cloud.src}
+          alt={cloud.alt}
+          width={cloud.width}
+          height={cloud.height}
+          style={{ position: "absolute", ...cloud.style }}
+          priority
+        />
+      ))}
 
       {/* Logo */}
-      <Image src="/images/logo.png" alt="logo" width={70} height={70} style={{ position: "absolute", top: 20, left: 20, zIndex: 2 }} />
+      <Image
+        src="/images/logo.png"
+        alt="MIC Logo"
+        width={70}
+        height={70}
+        style={{ position: "absolute", top: 20, left: 20, zIndex: 2 }}
+        priority
+      />
 
       {/* Pipes */}
-      <Image src="/images/pipe.png" alt="pipe" width={120} height={400} style={{ position: "absolute", top: 0, left: 160 }} />
-      <Image src="/images/pipe.png" alt="pipe" width={120} height={400} style={{ position: "absolute", top: 0, left: 420 }} />
-      <Image src="/images/pipe.png" alt="pipe" width={120} height={400} style={{ position: "absolute", top: 0, left: 650 }} />
-      <Image src="/images/pipe.png" alt="pipe" width={120} height={400} style={{ position: "absolute", top: 0, left: 900 }} />
+      {pipes.map((pipe, i) => (
+        <Image
+          key={i}
+          src="/images/pipe.png"
+          alt="Pipe"
+          width={120}
+          height={400}
+          style={{ position: "absolute", top: 0, left: pipe.left }}
+        />
+      ))}
 
-      {/* Social Icons*/}
-      <a href="mailto:your@email.com" target="_blank" rel="noopener noreferrer">
-        <Image src="/images/mail.png" alt="mail" width={60} height={45} style={{ position: "absolute", top: 180, left: 100, zIndex: 2 }} />
-      </a>
-      <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
-        <Image src="/images/instagram.png" alt="instagram" width={60} height={60} style={{ position: "absolute", top: 120, left: 470, zIndex: 2 }} />
-      </a>
-      <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
-        <Image src="/images/linkedin.png" alt="linkedin" width={60} height={60} style={{ position: "absolute", top: 80, left: 720, zIndex: 2 }} />
-      </a>
+      {/* Social Icons */}
+      {socialIcons.map((icon, i) => (
+        <a
+          key={i}
+          href={icon.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={icon.aria}
+          style={{ position: "absolute", ...icon.style }}
+        >
+          <Image src={icon.src} alt={icon.alt} width={icon.width} height={icon.height} />
+        </a>
+      ))}
 
       {/* Flappy Bird */}
-      <Image src="/images/bird.png" alt="bird" width={60} height={45} style={{ position: "absolute", top: 250, left: 220, zIndex: 2 }} />
+      <Image
+        src="/images/bird.png"
+        alt="Flappy Bird"
+        width={60}
+        height={45}
+        style={{ position: "absolute", top: 250, left: 220, zIndex: 2 }}
+      />
 
       {/* Menu Icon */}
-      <Image src="/images/menu.png" alt="menu" width={40} height={40} style={{ position: "absolute", bottom: 30, right: 30, zIndex: 2 }} />
+      <button
+        aria-label="Open menu"
+        style={{
+          position: "absolute",
+          bottom: 30,
+          right: 30,
+          zIndex: 2,
+          background: "none",
+          border: "none",
+          padding: 0,
+          cursor: "pointer",
+        }}
+        tabIndex={0}
+      >
+        <Image src="/images/menu.png" alt="Menu" width={40} height={40} />
+      </button>
     </div>
   );
 }
